dfb03d3fab1da7859cafd2d6f2434be9f5aa1dcf
BARBARA KUFFOUR <60520775+bkf1at@users.noreply.github.com>
2023-01-11 11:14:31 -0500

    First commit

---
+++ client/index.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('http://localhost:5000/getAll')
+        .then(response => response.json())
+        .then(data => loadHTMLTable(data['data']));
+    //loadHTMLTable([]);
+});
+
+const addBtn = document.querySelector('#add-name-btn');
+
+addBtn.onclick = function() {
+    const nameInput = document.querySelector('#name-input');
+    const name = nameInput.value;
+    nameInput.value = "";
+
+    fetch('http://localhost:5000/insert', {
+            headers: {
+                'Content-type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({ name: name })
+        })
+        .then(response => response.json())
+        .then(data => insertRowIntoTable(data['data']));
+}
+
+function insertRowIntoTable(data) {
+
+}
+
+function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody');
+
+    if (data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+        //return;
+    }
+
+    // let tableHtml = "";
+
+    // data.forEach(function({ id, name, date_added }) {
+    //     tableHtml += "<tr>";
+    //     tableHtml += '<td>${id}</td>';
+    //     tableHtml += '<td>${name}</td>';
+    //     tableHtml += '<td>${new Date (date_added).toLocaleString()}</td>';
+    //     tableHtml += '<td><button class="delete-row-btn" data-id=${id}>Delete</td>';
+    //     tableHtml += '<td><button class="edit-row-btn" data-id=${id}>Edit</td>';
+    //     tableHtml += "</tr>";
+    // });
+
+    // table.innerHtml = tableHtml;
+
+}
---
+++ server/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbService = require('./dbService');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// create
+app.post('/insert', (request, response) => {
+    //console.log(request.body);
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.insertNewName(name);
+
+    result
+        .then(data => response.json({ success: true }))
+        .catch(err => console.log(err));
+});
+
+// read
+app.get('/getAll', (request, response) => {
+    //response.json({
+    //success: true
+    //});
+    //console.log('test');
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+
+    result
+        .then(data => response.json({ data: data }))
+        .catch(err => console.log(err));
+})
+
+// update
+
+
+// delete
+
+app.listen(process.env.PORT, () => console.log('app is working'));
